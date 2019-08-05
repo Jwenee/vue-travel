86769158f09970ae994a9d8fc5ce38e26d35d4ce
jwen <283602670@qq.com>
2019-08-05 19:38:48 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import factClick from 'fastclick'
 import router from './router'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+// 解决移动端300ms点击延迟
+factClick.attach(document.body)
 
 new Vue({
   router,
