28c73342ae3fe23f015112bc9fe65da6f51a3f1b
jwen <283602670@qq.com>
2019-08-06 12:00:49 +0800

    add recommend

---
+++ src/components/Recommend.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <!-- 1px边框border-bottom引入的reset -->
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+
+        <img class="item-img" :src="item.imgUrl" alt="">
+
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data() {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1907/43/434697bdffd1475fa3.water.jpg_200x200_1184ab09.jpg',
+        title: '乐岛海洋王国',
+        desc: '山海关区'
+      },{
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1907/43/434697bdffd1475fa3.water.jpg_200x200_1184ab09.jpg',
+        title: '乐岛海洋王国',
+        desc: '山海关区'
+      },{
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1907/43/434697bdffd1475fa3.water.jpg_200x200_1184ab09.jpg',
+        title: '乐岛海洋王国',
+        desc: '山海关区'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eeeeee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      // min-width: 0让里面的内容超出后显示...
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #cccccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        color: #ffffff
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+
+</style>
---
+++ src/components/Weekend.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <!-- 1px边框border-bottom引入的reset -->
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" alt="">
+        </div>
+
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data() {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1812/58/ebdb4ae5c789be.jpg_r_640x214_21321088.jpg',
+        title: '乐岛海洋王国',
+        desc: '山海关区'
+      },{
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1812/58/ebdb4ae5c789be.jpg_r_640x214_21321088.jpg',
+        title: '乐岛海洋王国',
+        desc: '山海关区'
+      },{
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1812/58/ebdb4ae5c789be.jpg_r_640x214_21321088.jpg',
+        title: '乐岛海洋王国',
+        desc: '山海关区'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eeeeee
+    text-indent: .2rem
+  .item-img-wrapper
+    // 自动撑开里面的图片宽高
+    overflow: hidden
+    height: 0
+    padding-bottom: 33.9%
+    .item-img
+      width: 100%
+
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #cccccc
+      ellipsis()
+
+</style>
